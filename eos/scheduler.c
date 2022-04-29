a2f889f95a738860f3820af69a25cad2d1b35005
EdisonAgudelo <edison.agudelo@netuxtecnologia.com>
2022-04-29 08:39:47 -0500

    chore(scheduler): add simple comments to code. Also delete some redundacy in code

--- eos/scheduler.c
+++ eos/scheduler.c
@@ -23,10 +23,11 @@ SOFTWARE.
 */
 
 
-
 #include "scheduler.h" 
 
-
+//this allows critical section execution
+//if there is no a general function to do that, you can manually disables 
+//thos peripheral that may generate any EOS notification... for example the tick interrupt
 #ifndef portEOS_DISABLE_ISR
 #define portEOS_DISABLE_ISR()
 #endif
@@ -35,11 +36,14 @@ SOFTWARE.
 #define portEOS_ENABLE_ISR()
 #endif
 
+//how many individual priorities is gonna exist. Task may share same priority number
 #ifndef EOS_MAX_TASK_PRIORITY
 #define EOS_MAX_TASK_PRIORITY 2
 #endif
 
+//task are handled in list... so the next macros is for list handling
 
+//Add a task to list tail
 #define EOS_ADD_TO_LIST(list, task)     \
     if((list).head == NULL)             \
     {                                   \
@@ -59,7 +63,8 @@ SOFTWARE.
     {                                   \
         EOS_ASSERT(false);                   \
     }
-    
+
+//remove a task from a list
 #define EOS_REMOVE_FROM_LIST(list, task)        \
     if((task)->prev == NULL)                    \
     {                                           \
@@ -83,14 +88,23 @@ SOFTWARE.
     (task)->prev = NULL;                        \
     (task)->belong_to_list = NULL
 
+//index is "list depending" meaning. Eg. for ready_list
+//it holds the next task to execute to allows true yielding 
+//even when higer priority list preempt actual list
 #define EOS_SET_LIST_INDEX(list, task)          \
     (list).index = task
 
+//the starting point of the list
 #define EOS_GET_HEAD_FROM_LIST(list)     (list).head
+//to go foward
 #define EOS_GET_NEXT_FROM_ITEM(task)     (task)->next
+//to go backward
 #define EOS_GET_PREV_FROM_ITEM(task)     (task)->prev
 
+//insert operations
 
+//intem <=> item <=> ref_task <=> item <=> item
+//intem <=> item <=> ref_task <=> new_task <=> item <=> item
 #define EOS_INSERT_NEXT_TO_ITEM_IN_LIST(list, ref_task, new_task)   \
     if((ref_task) != NULL)                                          \
     {                                                               \
@@ -104,6 +118,8 @@ SOFTWARE.
             (new_task)->next->prev = new_task;                      \
     }
 
+//intem <=> item <=> ref_task <=> item <=> item
+//intem <=> item <=> new_task <=> ref_task <=> item <=> item
 #define EOS_INSERT_PREV_TO_ITEM_IN_LIST(list, new_task, ref_task)   \
     if((ref_task) != NULL)                                          \
     {                                                               \
@@ -116,29 +132,38 @@ SOFTWARE.
         else                                                        \
             (new_task)->prev->next = new_task;                      \
     }
-    
+
+//calculate how many ticks is left even if ticks overflow //useful for long term applications
 #define EOS_DELAY_REMAIN(unblock_tick)                      \
     (eos_tick <= (unblock_tick) ?                           \
     ((unblock_tick) - eos_tick) :                           \
     ((0xffffffff - (eos_tick - (unblock_tick))) + 1))
 
-
+//list body
 typedef struct {
     EOSTaskT head;
     EOSTaskT tail;
     EOSTaskT index;
 }EOSListT;
 
+//holds the actual tick of system
 uint32_t eos_tick = 0;
+//holds the actual executing task
 EOSTaskT eos_running_task = NULL;
 
 //0 is also a valid priority
+//holds all tasks that are ready to execute. 
 EOSListT ready_list[EOS_MAX_TASK_PRIORITY + 1] = {{NULL, NULL, NULL}};
+//holds all tasks that have a bloking event with a finite timeout
+//list head holds the task with less expire time and list tail holds the tasks
+//with the great expire time.
 EOSListT blocked_list = {NULL, NULL, NULL};
+//holds all tasks that have been blocked but have an infinite timeout or was
+//directly suspended
 EOSListT suspended_list = {NULL, NULL, NULL};
 
 
-
+//increments actual tick value and unblock those tasks that have reach maximum block time
 void EOSTickIncrement(void)
 {
     EOSTaskT delayed_task;
@@ -151,9 +176,12 @@ void EOSTickIncrement(void)
         delayed_task = EOS_GET_HEAD_FROM_LIST(blocked_list);
         
         if(delayed_task == NULL)
-            break; //no delayed task
+            break; //no blocked tasks
             
         //check if its time to unblock
+        
+        //if overflow we have to wait until that overflow "is cleared"
+        //overflow happen when unblock_tick is less that actual tick count
         if(delayed_task->tick_over_flow)
         {
             if(delayed_task->unblock_tick >= eos_tick)
@@ -166,10 +194,10 @@ void EOSTickIncrement(void)
             {
                 EOS_REMOVE_FROM_LIST(blocked_list, delayed_task);
                 EOS_ADD_TO_LIST(ready_list[delayed_task->priority], delayed_task);
-                delayed_task->ticks_to_delay = 0; //basically signals timeout
+                delayed_task->ticks_to_delay = 0; //signals timeout
             } else 
             {
-                break; //we just finish to unblock task
+                break; //we just finish to unblock task waiting for the actual tick
             }
         }
         
@@ -180,7 +208,9 @@ void EOSTickIncrement(void)
     
 }
 
-
+//minimum executing unit
+//this keeps scheduler always doing something if no task are ready... 
+//otherwhise, it will crash
 static void EOSIdleTask(EOSStackT stack, void *args)
 {
     EOS_INIT(stack);
@@ -195,17 +225,14 @@ static void EOSIdleTask(EOSStackT stack, void *args)
 }
 
 
-static EOSTaskT EOSGetNextTaskToRun(EOSTaskT actual_task)
+//found the next task to execute.
+//it prioritized task and allows yielding "with the index pointer"
+static EOSTaskT EOSGetNextTaskToRun(void)
 {
     EOSTaskT task_to_run;
     uint8_t priority = EOS_MAX_TASK_PRIORITY;
     
-    /*
-        if actual_task is in the current most prioritized list
-        this loop should found the owner list of the actual_task. So to no affect yield 
-        operations, we return te actual_task (which is a task that was preselected to
-        to run, but it isn't executed yet)
-    */
+
     portEOS_DISABLE_ISR();
     do {
         task_to_run = EOS_GET_HEAD_FROM_LIST(ready_list[priority]);
@@ -213,10 +240,7 @@ static EOSTaskT EOSGetNextTaskToRun(EOSTaskT actual_task)
             
             portEOS_ENABLE_ISR();
             
-            if(actual_task == NULL || actual_task->priority < priority)
-                return ((ready_list[priority].index == NULL) ? task_to_run : ready_list[priority].index);
-                
-            return actual_task;
+            return ((ready_list[priority].index == NULL) ? task_to_run : ready_list[priority].index);
         }
     } while(priority--);
     
@@ -228,9 +252,9 @@ static EOSTaskT EOSGetNextTaskToRun(EOSTaskT actual_task)
     return NULL;
 }
 
+//the Scheduler itself. It is the only one allowed to remove task from de the ready list and add to others list
 void EOSScheduler(void)
 {
-    EOSTaskT process_task = NULL;
     EOSTaskT index_task = NULL;
   
     
@@ -241,27 +265,26 @@ void EOSScheduler(void)
 
     while(eos_tick < 40)
     {
-        //check it this precanditade it's the most prioritized
-        eos_running_task = EOSGetNextTaskToRun(eos_running_task);
+        eos_running_task = EOSGetNextTaskToRun();
         
         eos_running_task->task(eos_running_task->stack, eos_running_task->args);
         EOS_ASSERT(!EOSCheckOverFlow(eos_running_task->stack, eos_running_task->stack_size));
         
-        //before any removal... get the next one
-        process_task = eos_running_task;
         
         portEOS_DISABLE_ISR();
         
-        //get the nex precanditade to run in the same priority level
-        eos_running_task = EOS_GET_NEXT_FROM_ITEM(process_task);
-        //we save which is de actual "first" element of the list... useful for true yielding
-        //even wen there is multiple preemption
-        EOS_SET_LIST_INDEX(ready_list[process_task->priority], eos_running_task);
+        //before any removal... save the next task to execute
         
-        switch(*((EOSTaskStateT *)process_task->stack))
+        //get the nex canditade to run in the same priority level
+        //we save this as the actual "first" element of the list... useful for true yielding
+        //even wen there is multiple preemption
+        EOS_SET_LIST_INDEX(ready_list[eos_running_task->priority], EOS_GET_NEXT_FROM_ITEM(eos_running_task));
+        
+        //then we can update task state moving through list owner
+        switch(*((EOSTaskStateT *)eos_running_task->stack))
         {
             case kEOSTaskEnded://the task/function ended all its routines
-                EOS_REMOVE_FROM_LIST(ready_list[process_task->priority], process_task);
+                EOS_REMOVE_FROM_LIST(ready_list[eos_running_task->priority], eos_running_task);
                 break;
             
             //just go ahead
@@ -270,29 +293,29 @@ void EOSScheduler(void)
                 break;
             case kEOSTaskBlocked:
             
-                EOS_REMOVE_FROM_LIST(ready_list[process_task->priority], process_task);
+                EOS_REMOVE_FROM_LIST(ready_list[eos_running_task->priority], eos_running_task);
                 
-                process_task->unblock_tick = eos_tick + process_task->ticks_to_delay;
-                process_task->tick_over_flow = process_task->unblock_tick < eos_tick;
+                eos_running_task->unblock_tick = eos_tick + eos_running_task->ticks_to_delay;
+                eos_running_task->tick_over_flow = eos_running_task->unblock_tick < eos_tick;
                     
                 
-                //lock for a task that have more delay than process_task
+                //lock for a task that have more delay than eos_running_task
                 for(index_task = EOS_GET_HEAD_FROM_LIST(blocked_list); index_task != NULL; index_task = EOS_GET_NEXT_FROM_ITEM(index_task))
                 {
-                    if(EOS_DELAY_REMAIN(index_task->unblock_tick)>EOS_DELAY_REMAIN(process_task->unblock_tick))
+                    if(EOS_DELAY_REMAIN(index_task->unblock_tick)>EOS_DELAY_REMAIN(eos_running_task->unblock_tick))
                         break;
                 }
                 if(index_task == NULL)
                 {
-                    EOS_ADD_TO_LIST(blocked_list, process_task);
+                    EOS_ADD_TO_LIST(blocked_list, eos_running_task);
                 } else {
-                    EOS_INSERT_PREV_TO_ITEM_IN_LIST(blocked_list, process_task, index_task);
+                    EOS_INSERT_PREV_TO_ITEM_IN_LIST(blocked_list, eos_running_task, index_task);
                 }
                 
                 break;
             case kEOSTaskSuspended:
-                EOS_REMOVE_FROM_LIST(ready_list[process_task->priority], process_task);
-                EOS_ADD_TO_LIST(suspended_list, process_task);
+                EOS_REMOVE_FROM_LIST(ready_list[eos_running_task->priority], eos_running_task);
+                EOS_ADD_TO_LIST(suspended_list, eos_running_task);
                 break;
             
             default:
@@ -300,13 +323,17 @@ void EOSScheduler(void)
         }
         portEOS_ENABLE_ISR();
         
-        //virtual tick
+        //virtual tick for test purposes
         EOSTickIncrement();
     }
 }
 
-EOSTaskT EOSCreateStaticTask(EOSTask task, void *args, uint8_t priority, uint32_t stack_size, EOSStaticStack *stack_buffer, EOSStaticTaskT *task_buffer)
+
+//init a task and add it to the ready list according to priority
+EOSTaskT EOSCreateStaticTask(EOSTaskFunction task, void *args, uint8_t priority, uint32_t stack_size, EOSStaticStack *stack_buffer, EOSStaticTaskT *task_buffer)
 {
+    portEOS_DISABLE_ISR();
+    
     task_buffer->stack = stack_buffer;
     task_buffer->stack_size = stack_size;
     task_buffer->args = args;
@@ -317,6 +344,8 @@ EOSTaskT EOSCreateStaticTask(EOSTask task, void *args, uint8_t priority, uint32_
 
     EOS_INIT_STACK(stack_buffer, stack_size);
     EOS_ADD_TO_LIST(ready_list[task_buffer->priority], task_buffer);
+    
+    portEOS_ENABLE_ISR();
     
     return task_buffer;
 }
